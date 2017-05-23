9ec58dfc179dc263ccb82fd7e2a5a4ae8d1064df
jszheng <zheng_js@hotmail.com>
2017-05-23 10:15:30 +0800

    pass cifar10 full training with CPU

--- caffe/cifar10/train_full.sh
+++ caffe/cifar10/train_full.sh
@@ -2,7 +2,7 @@
 caffe train --solver=cifar10_full_solver.prototxt $@
 
 # reduce learning rate by factor of 10
-caffe train --solver=cifar10_full_solver_lr1.prototxt --snapshot=cifar10_full_iter_60000.solverstate $@
+caffe train --solver=cifar10_full_solver_lr1.prototxt --snapshot=cifar10_full_iter_60000.solverstate.h5 $@
 
 # reduce learning rate by factor of 10
-caffe train --solver=cifar10_full_solver_lr2.prototxt --snapshot=cifar10_full_iter_65000.solverstate $@
+caffe train --solver=cifar10_full_solver_lr2.prototxt --snapshot=cifar10_full_iter_65000.solverstate.h5 $@
